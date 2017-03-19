a4c1fc72145b58736ea53785f41a3ab5946c1812
Dienst <dienst-je@wacker.corp>
2017-03-19 12:46:04 +0100

    change Version

--- check_mk_raw/start.sh
+++ check_mk_raw/start.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-echo "omd-labs: Starting site $SITENAME..."
+rm -rf /var/run/httpd/httpd.pid
+omd stop cmk
+
+if [ -n $LIVESTATUS_TCP ]
+then
+  if [ "$LIVESTATUS_TCP" = "on" ]
+  then
+    echo "Enable Livestatus"
+    omd config cmk set LIVESTATUS_TCP on
+  elif [ "$LIVESTATUS_TCP" == "off" ]
+  then
+    echo "Disable Livestatus"
+    omd config cmk set LIVESTATUS_TCP off
+  else
+    echo "Disable Livestatus"
+    omd config cmk set LIVESTATUS_TCP off
+  fi
+else
+  echo "Disable Livestatus"
+  omd config cmk set LIVESTATUS_TCP off
+fi
+
+
+echo "omd: Starting site $SITENAME..."
 echo "--------------------------------------"
 omd start $SITENAME
 echo "--------------------------------------"
 
-echo "omd-labs: Starting Apache web server..."
+echo "omd: Starting Apache web server..."
 exec /usr/sbin/httpd -D FOREGROUND
